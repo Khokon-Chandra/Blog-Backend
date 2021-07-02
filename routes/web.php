4a143bb29350e702679a2ba267a0088f2e74a8c5
Khokon Chandra <cram3632@gmail.com>
2021-07-02 10:49:07 +0600

    some changes on route

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,29 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\PostController;
+
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
 
-
 Route::group(['middleware'=>'auth'],function(){
 
-    Route::get('/', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/',[IndexController::class,'dashboard'])->name('dashboard');
 
-    /**
-     * Post group is appear here
-     */
     Route::prefix('posts')->group(function () {
-           
         Route::get('/',[PostController::class,'index'])->name('post');
-        
-        Route::get('/{post:slug}',[PostController::class,'edit'])->name('post.update');
-        Route::post('/{post:slug}',[PostController::class,'update']);
-
         Route::get('/add-new',[PostController::class,'create'])->name('post.add-new');
         Route::post('/add-new',[PostController::class,'store']);
-        
+        Route::get('/{post:slug}',[PostController::class,'edit'])->name('post.update');
+        Route::post('/{post:slug}',[PostController::class,'update']);
         Route::post('/delete',[PostController::class,'destroy'])->name('post.delete');
         Route::get('/category',[CategoryController::class,'create'])->name('category');
     });
-
-     /**
-     * Users group is appear here
-     */
 
     Route::prefix('users')->group(function () {
         Route::get('/',[UserController::class,'index'])->name('user');

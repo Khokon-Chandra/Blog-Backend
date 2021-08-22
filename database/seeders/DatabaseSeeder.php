11ab6e93d066821deea9db7603215d22be4aae90
khokon-chandra <cram3632@gmail.com>
2021-08-22 15:22:19 +0600

    mailable class created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create(['username'=>'khokon1234','name'=>'Khokon Chandra','role'=>1,'email'=>'cram3632@gmail.com','password'=>Hash::make('00000000')]);
         User::factory()->create();
         $this->call([
             CategorySeeder::class,
